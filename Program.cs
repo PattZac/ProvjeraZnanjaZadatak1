9279dabb316bd267617883ae486d91d88bd4e667
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-25 08:54:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvjeraZnanjaZadatak1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi niz znakova: ");
+            string s = Console.ReadLine();
+            s = s.Replace(' ','_');
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
